e080bcb6ad7befadae33a74811435a7f237fda0c
BenHigginbottom <ben.higginbottom@gmail.com>
2018-01-26 14:55:58 +0000

    local state management now complete and working (as tested)

--- TwitterBot_QEAC.py
+++ TwitterBot_QEAC.py
@@ -11,11 +11,6 @@ CONSUMER_SECRET = "CONSUMER_SECRET"
 ACCESS_TOKEN = "ACCESS_TOKEN"
 ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET"
 
-#Edit these to the user and comment you want to make
-#Don't put the @ in the user
-#userdispname = "SET ME TO THE USER YOU WANT TO QUOTE"
-#comment = "SET ME TO THE COMMENT"
-
 #Search Terms
 query = "Queen Elizabeth OR Aircraft Carrier AND jet OR aircraft OR leak OR sink"
 
@@ -56,36 +51,34 @@ else:
 #Search Twitter
 search = api.search(q = query, count = 100, since_id = lastquoteid, include_rts = False, exclude_replies = False)
 
+#And now, the logic
+idarray = []
+for twid in search:
+    idarray.append(twid.id)
 
-#And now the logic and the update/post
-#for stuff in lasttweet:
-#    lastid = str(stuff.id)
-#    if lastquoteid == lastid:
-#        break
-#    else:
-#        try:
-#            quoteurl = baseurl + lastid
-#            response = " " + comment + " %s " % quoteurl
-#            s = api.update_status(response)
-#            f = open('./lastquoteid.txt', 'w')
-#            f.write(lastid)
-#            f.close()
-#        except tweepy.TweepError as e:
-#            break
-
-for s in search:
-    for a in aircraft:
-        if a in s.text:
-            screen_name = s.user.screen_name
-            response = "@%s Helicopters are aircraft" % screen_name
-            s = api.update_status(response, s.id)
-    for j in jets:
-        if j in s.text:
-            screen_name = s.user.screen_name
-            response = "@%s The F35 testing begins later this year" % screen_name
-            s = api.update_status(response, s.id)
-    for l in leaking:
-        if l in s.text:
-            screen_name = s.user.screen_name
-            response = "@%s This is what sea trials are for, issues will be found and fixed!" % screen_name
-            s = api.update_status(response, s.id)
+lastid =  str(idarray[0])
+if lastquoteid == lastid:
+    print "No New Tweets Found"
+else:
+    f = open('./lastquoteid.txt', 'w')
+    f.write(lastid)
+    f.close()
+    try:
+        for s in search:
+            for a in aircraft:
+                if a in s.text:
+                    screen_name = s.user.screen_name
+                    response = "@%s Helicopters are aircraft" % screen_name
+                    s = api.update_status(response, s.id)
+            for j in jets:
+                if j in s.text:
+                    screen_name = s.user.screen_name
+                    response = "@%s The F35 testing begins later this year" % screen_name
+                    s = api.update_status(response, s.id)
+            for l in leaking:
+                if l in s.text:
+                    screen_name = s.user.screen_name
+                    response = "@%s This is what sea trials are for, issues will be found and fixed!" % screen_name
+                    s = api.update_status(response, s.id)
+    except tweepy.TweepError as e:
+        print e
